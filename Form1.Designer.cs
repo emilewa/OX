86f2f29878838c3ec9705e5842ce14d86fa98a56
emilewa <45316859+emilewa@users.noreply.github.com>
2019-05-05 19:08:24 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace OX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.p1 = new System.Windows.Forms.Button();
+            this.p2 = new System.Windows.Forms.Button();
+            this.p3 = new System.Windows.Forms.Button();
+            this.p4 = new System.Windows.Forms.Button();
+            this.p5 = new System.Windows.Forms.Button();
+            this.p6 = new System.Windows.Forms.Button();
+            this.p7 = new System.Windows.Forms.Button();
+            this.p8 = new System.Windows.Forms.Button();
+            this.p9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::OX.Properties.Resources.nowagra2;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("AR DESTINE", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "NOWA GRA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1.Enabled = false;
+            this.p1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p1.Location = new System.Drawing.Point(12, 164);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(68, 64);
+            this.p1.TabIndex = 1;
+            this.p1.Text = "1";
+            this.p1.UseVisualStyleBackColor = false;
+            this.p1.Click += new System.EventHandler(this.p1_Click);
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2.Enabled = false;
+            this.p2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p2.Location = new System.Drawing.Point(86, 164);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(66, 64);
+            this.p2.TabIndex = 2;
+            this.p2.Text = "2";
+            this.p2.UseVisualStyleBackColor = false;
+            this.p2.Click += new System.EventHandler(this.p2_Click);
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Transparent;
+            this.p3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p3.Enabled = false;
+            this.p3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p3.Location = new System.Drawing.Point(158, 164);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(66, 64);
+            this.p3.TabIndex = 3;
+            this.p3.Text = "3";
+            this.p3.UseVisualStyleBackColor = false;
+            this.p3.Click += new System.EventHandler(this.p3_Click);
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.Transparent;
+            this.p4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p4.Enabled = false;
+            this.p4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p4.Location = new System.Drawing.Point(12, 240);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(68, 61);
+            this.p4.TabIndex = 4;
+            this.p4.Text = "4";
+            this.p4.UseVisualStyleBackColor = false;
+            this.p4.Click += new System.EventHandler(this.p4_Click);
+            // 
+            // p5
+            // 
+            this.p5.BackColor = System.Drawing.Color.Transparent;
+            this.p5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p5.Enabled = false;
+            this.p5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p5.Location = new System.Drawing.Point(86, 240);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(66, 61);
+            this.p5.TabIndex = 5;
+            this.p5.Text = "5";
+            this.p5.UseVisualStyleBackColor = false;
+            this.p5.Click += new System.EventHandler(this.p5_Click);
+            // 
+            // p6
+            // 
+            this.p6.BackColor = System.Drawing.Color.Transparent;
+            this.p6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p6.Enabled = false;
+            this.p6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p6.Location = new System.Drawing.Point(158, 240);
+            this.p6.Name = "p6";
+            this.p6.Size = new System.Drawing.Size(66, 61);
+            this.p6.TabIndex = 6;
+            this.p6.Text = "6";
+            this.p6.UseVisualStyleBackColor = false;
+            this.p6.Click += new System.EventHandler(this.p6_Click);
+            // 
+            // p7
+            // 
+            this.p7.BackColor = System.Drawing.Color.Transparent;
+            this.p7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p7.Enabled = false;
+            this.p7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p7.Location = new System.Drawing.Point(13, 315);
+            this.p7.Name = "p7";
+            this.p7.Size = new System.Drawing.Size(67, 65);
+            this.p7.TabIndex = 7;
+            this.p7.Text = "7";
+            this.p7.UseVisualStyleBackColor = false;
+            this.p7.Click += new System.EventHandler(this.p7_Click);
+            // 
+            // p8
+            // 
+            this.p8.BackColor = System.Drawing.Color.Transparent;
+            this.p8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p8.Enabled = false;
+            this.p8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p8.Location = new System.Drawing.Point(86, 315);
+            this.p8.Name = "p8";
+            this.p8.Size = new System.Drawing.Size(66, 65);
+            this.p8.TabIndex = 8;
+            this.p8.Text = "8";
+            this.p8.UseVisualStyleBackColor = false;
+            this.p8.Click += new System.EventHandler(this.p8_Click);
+            // 
+            // p9
+            // 
+            this.p9.BackColor = System.Drawing.Color.Transparent;
+            this.p9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p9.Enabled = false;
+            this.p9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.p9.Location = new System.Drawing.Point(158, 315);
+            this.p9.Name = "p9";
+            this.p9.Size = new System.Drawing.Size(66, 65);
+            this.p9.TabIndex = 9;
+            this.p9.Text = "9";
+            this.p9.UseVisualStyleBackColor = false;
+            this.p9.Click += new System.EventHandler(this.p9_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("AR DESTINE", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 37);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Witaj w grze OX  :)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = global::OX.Properties.Resources.tlo3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(236, 392);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p9);
+            this.Controls.Add(this.p8);
+            this.Controls.Add(this.p7);
+            this.Controls.Add(this.p6);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(252, 431);
+            this.MinimumSize = new System.Drawing.Size(252, 431);
+            this.Name = "Form1";
+            this.Text = "Gra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button p1;
+        private System.Windows.Forms.Button p2;
+        private System.Windows.Forms.Button p3;
+        private System.Windows.Forms.Button p4;
+        private System.Windows.Forms.Button p5;
+        private System.Windows.Forms.Button p6;
+        private System.Windows.Forms.Button p7;
+        private System.Windows.Forms.Button p8;
+        private System.Windows.Forms.Button p9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OX
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public int idgracza = 1;
+        public int[] gra = new int[10];
+        public int wygrana;
+        public Form1()
+        {
+            InitializeComponent();
+            nowagra();
+        }
+        private Boolean sprawdz()
+        {
+           Boolean odp = false;
+
+            for(int i=1,j=1; i<=3; i++)
+            {
+                
+                if (gra[j] == gra[j + 1] && gra[j + 1] == gra[j + 2] && gra[j]!=0)
+                    odp = true;
+                else
+                if (gra[i] == gra[i + 3] && gra[i + 3] == gra[i + 6] && gra[i]!=0)
+                    odp = true;
+
+                j = j + 3;
+            }
+
+            if (gra[1] == gra[5] && gra[5] == gra[9] && gra[1] != 0)
+                    odp = true;
+            if (gra[3] == gra[5] && gra[5] == gra[7] && gra[7] != 0)
+                    odp = true;
+
+            return odp;
+        }
+
+        private void nowagra()
+        {
+            for (int i = 1; i<=9 ; ++i)
+            {
+                gra[i] = 0;
+
+            }
+
+            p1.Text = " ";
+            p2.Text = " ";
+            p3.Text = " ";
+            p4.Text = " ";
+            p5.Text = " ";
+            p6.Text = " ";
+            p7.Text = " ";
+            p8.Text = " ";
+            p9.Text = " ";
+
+            p1.BackgroundImage = null;
+            p2.BackgroundImage = null;
+            p3.BackgroundImage = null;
+            p4.BackgroundImage = null;
+            p5.BackgroundImage = null;
+            p6.BackgroundImage = null;
+            p7.BackgroundImage = null;
+            p8.BackgroundImage = null;
+            p9.BackgroundImage = null;
+
+            p1.Enabled = true;
+            p2.Enabled = true;
+            p3.Enabled = true;
+            p4.Enabled = true;
+            p5.Enabled = true;
+            p6.Enabled = true;
+            p7.Enabled = true;
+            p8.Enabled = true;
+            p9.Enabled = true;
+
+            label1.ForeColor = Color.Black;
+            label1.Text = "Ruch gracza: " + idgracza;
+        }
+
+        private Boolean remis()
+        {
+            Boolean stan = true;
+            for(int i = 1; i<=9; ++i)
+            {
+                if (gra[i] == 0) stan = false;
+            }
+            return stan;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nowagra();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nieklik()
+        {
+            p1.Enabled = false;
+            p2.Enabled = false;
+            p3.Enabled = false;
+            p4.Enabled = false;
+            p5.Enabled = false;
+            p6.Enabled = false;
+            p7.Enabled = false;
+            p8.Enabled = false;
+            p9.Enabled = false;
+        }
+
+        public Image O = new Bitmap(@"C:\\Users\\Emilia\\Documents\\Visual Studio 2015\\Projects\\Gra_OX\\OX\\omin.png");
+        public Image X = new Bitmap(@"C:\\Users\\Emilia\\Documents\\Visual Studio 2015\\Projects\\Gra_OX\\OX\\xmin.png");
+
+        private void obr(string x)
+        {
+
+            
+        }
+
+
+        private void p1_Click(object sender, EventArgs e)
+        {
+    
+
+            gra[1] = idgracza;
+            p1.Text = "" + gra[1];
+            if (idgracza == 1)
+                p1.BackgroundImage = O;
+            else p1.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz "+idgracza+" !!!";
+                nieklik();
+            }
+            else
+            {
+            if (idgracza == 1)
+                idgracza = 2;
+            else idgracza = 1;
+            label1.Text = "Ruch gracza: " + idgracza;
+            p1.Enabled = false;
+            }
+
+            if (remis())
+                label1.Text = "Remis ;/";
+
+
+
+        }
+
+        private void p2_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            gra[2] = idgracza;
+            p2.Text = "" + gra[2];
+            if (idgracza == 1)
+                p2.BackgroundImage = O;
+            else p2.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+                if (idgracza == 1)
+                idgracza = 2;
+                else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                p2.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+            }
+            
+
+        }
+
+        private void p3_Click(object sender, EventArgs e)
+        {
+            gra[3] = idgracza;
+            p3.Text = "" + gra[3];
+            if (idgracza == 1)
+                p3.BackgroundImage = O;
+            else p3.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+                 if (idgracza == 1)
+                    idgracza = 2;
+                    else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                p3.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+            }
+
+            
+        }
+
+        private void p4_Click(object sender, EventArgs e)
+        {
+            gra[4] = idgracza;
+            p4.Text = "" + gra[4];
+            if (idgracza == 1)
+                p4.BackgroundImage = O;
+            else p4.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+                if (idgracza == 1)
+                    idgracza = 2;
+                    else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                p4.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+
+            }
+
+            
+        }
+
+        private void p5_Click(object sender, EventArgs e)
+        {
+            gra[5] = idgracza;
+            p5.Text = "" + gra[5];
+            if (idgracza == 1)
+                p5.BackgroundImage = O;
+            else p5.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+               if (idgracza == 1)
+                    idgracza = 2;
+                else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                p5.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+            }
+
+            
+        }
+
+        private void p6_Click(object sender, EventArgs e)
+        {
+            gra[6] = idgracza;
+            p6.Text = "" + gra[6];
+            if (idgracza == 1)
+                p6.BackgroundImage = O;
+            else p6.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+                if (idgracza == 1)
+                    idgracza = 2;
+                else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                p6.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+            }
+
+            
+
+        }
+
+        private void p7_Click(object sender, EventArgs e)
+        {
+            gra[7] = idgracza;
+            p7.Text = ""+gra[7];
+            if (idgracza == 1)
+                p7.BackgroundImage = O;
+            else p7.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+                if (idgracza == 1)
+                    idgracza = 2;
+                else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                p7.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+            }
+
+       
+        }
+
+        private void p8_Click(object sender, EventArgs e)
+        {
+            gra[8] = idgracza;
+            p8.Text = "" + gra[8];
+            if (idgracza == 1)
+                p8.BackgroundImage = O;
+            else p8.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+                if (idgracza == 1)
+                     idgracza = 2;
+                    else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                 p8.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+            }
+            
+
+        }
+
+        private void p9_Click(object sender, EventArgs e)
+        {
+
+            gra[9] = idgracza;
+            p9.Text = "" + gra[9];
+            if (idgracza == 1)
+                p9.BackgroundImage = O;
+            else p9.BackgroundImage = X;
+            if (sprawdz())
+            {
+                label1.BorderStyle = BorderStyle.Fixed3D;
+                label1.ForeColor = Color.Blue;
+                label1.Text = "Wygrywa gracz " + idgracza + " !!!";
+                nieklik();
+            }
+            else
+            {
+            if (idgracza == 1)
+                idgracza = 2;
+            else idgracza = 1;
+                label1.Text = "Ruch gracza: " + idgracza;
+                p9.Enabled = false;
+                if (remis())
+                label1.Text = "Remis ;/";
+            }
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
